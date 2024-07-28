11dafb9360f4b344134e5580b7724a7350d76798
cptknvckles <cptknvckles@protonmail.com>
2024-07-28 14:59:14 -0500

    modified main file

--- 100DaysOfPython/day10_calc/calc.py
+++ 100DaysOfPython/day10_calc/calc.py
@@ -14,20 +14,24 @@ operations = {
     '*': multiply,
     '/': divide
 }
-single_line = []
-num1 = int(input("Enter first number: "))
-for operands in operations:
-    single_line.append(operands)
-print(" ".join(single_line))
-keep_going = True
-while keep_going:
-    operand_symbol = input('choose operation from list above: ')
-    num2 = int(input("Enter next number: "))
-    answer = operations[operand_symbol](num1,num2)
-    print(f"{num1} {operand_symbol} {num2} = {answer}")
+#should put this in a function
+def calculator():
+    single_line = []
+    num1 = int(input("Enter first number: "))
+    for operands in operations:
+        single_line.append(operands)
+    print(" ".join(single_line))
+    keep_going = True
+    while keep_going:
+        operand_symbol = input('choose operation again: ')
+        num2 = int(input("Enter next number: "))
+        answer = operations[operand_symbol](num1,num2)
+        print(f"{num1} {operand_symbol} {num2} = {answer}")
 
-    if input(f'y/n continue with {answer}, or exit: ') == 'y':
-        num1 = answer
-    else:
-        print('see ya!')
-        keep_going = False
+        if input(f'y/n continue with {answer}, or exit: ') == 'y':
+            num1 = answer
+        else:
+            print('see ya!')
+            keep_going = False
+            calculator()
+calculator()
